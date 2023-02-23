f41a44c2ae784068b2004e3d68ada05fde824641
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-02-23 23:23:33 +0900

    Add ListPullRequests

--- infrastructure/github/github_test.go
+++ infrastructure/github/github_test.go
@@ -9,11 +9,11 @@ import (
 	"net/url"
 	"strings"
 	"testing"
-
-	"github.com/google/go-github/v50/github"
-	"github.com/nao1215/leadtime/domain/model"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-github/v50/github"
+	"github.com/nao1215/leadtime/domain/model"
 )
 
 func TestListRepositories(t *testing.T) {
@@ -185,4 +185,325 @@ func TestListRepositories(t *testing.T) {
 			t.Errorf("mismatch expect=%d, got=%d", apiErr.StatusCode, http.StatusUnauthorized)
 		}
 	})
+}
+
+func TestClient_ListPullRequests(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Get PR list", func(t *testing.T) {
+		t.Parallel()
+
+		now := time.Date(2023, 2, 24, 12, 34, 56, 0, time.UTC)
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			wantURL := "/repos/owner/repo/pulls"
+			if wantURL != req.URL.Path {
+				t.Errorf("mismatch want=%v, got=%s", wantURL, req.URL.Path)
+			}
+
+			wantHTTPMethod := "GET"
+			if wantHTTPMethod != req.Method {
+				t.Errorf("mismatch want=%v, got=%s", wantHTTPMethod, req.Method)
+			}
+
+			respBody, err := json.Marshal([]github.PullRequest{
+				{
+					ID:     github.Int64(1),
+					Number: github.Int(1),
+					State:  github.String("open"),
+					Title:  github.String("test_pr1"),
+					CreatedAt: &github.Timestamp{
+						Time: now,
+					},
+					ClosedAt: &github.Timestamp{
+						Time: now,
+					},
+					MergedAt: &github.Timestamp{
+						Time: now,
+					},
+					User: &github.User{
+						Name: github.String("test_user1"),
+					},
+					Comments:     github.Int(0),
+					Additions:    github.Int(10),
+					Deletions:    github.Int(5),
+					ChangedFiles: github.Int(2),
+				},
+				{
+					ID:     github.Int64(2),
+					Number: github.Int(2),
+					State:  github.String("closed"),
+					Title:  github.String("test_pr2"),
+					CreatedAt: &github.Timestamp{
+						Time: now,
+					},
+					ClosedAt: &github.Timestamp{
+						Time: now,
+					},
+					MergedAt: &github.Timestamp{
+						Time: now,
+					},
+					User: &github.User{
+						Name: github.String("test_user2"),
+					},
+					Comments:     github.Int(2),
+					Additions:    github.Int(5),
+					Deletions:    github.Int(2),
+					ChangedFiles: github.Int(1),
+				},
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+			rw.WriteHeader(http.StatusOK)
+			if _, err := rw.Write(respBody); err != nil {
+				t.Fatal(err)
+			}
+		}))
+		defer testServer.Close()
+
+		token := "token"
+		client := NewClient(token)
+		ctx := context.Background()
+
+		testURL, err := url.Parse(testServer.URL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		client.client.BaseURL = testURL
+		if !strings.HasSuffix(client.client.BaseURL.Path, "/") {
+			client.client.BaseURL.Path += "/"
+		}
+
+		wantPRs := []*model.PullRequest{
+			{
+				ID:           github.Int64(1),
+				Number:       github.Int(1),
+				State:        github.String("open"),
+				Title:        github.String("test_pr1"),
+				CreatedAt:    &model.Timestamp{Time: now},
+				ClosedAt:     &model.Timestamp{Time: now},
+				MergedAt:     &model.Timestamp{Time: now},
+				User:         &model.User{Name: github.String("test_user1")},
+				Comments:     github.Int(0),
+				Additions:    github.Int(10),
+				Deletions:    github.Int(5),
+				ChangedFiles: github.Int(2),
+			},
+			{
+				ID:           github.Int64(2),
+				Number:       github.Int(2),
+				State:        github.String("closed"),
+				Title:        github.String("test_pr2"),
+				CreatedAt:    &model.Timestamp{Time: now},
+				ClosedAt:     &model.Timestamp{Time: now},
+				MergedAt:     &model.Timestamp{Time: now},
+				User:         &model.User{Name: github.String("test_user2")},
+				Comments:     github.Int(2),
+				Additions:    github.Int(5),
+				Deletions:    github.Int(2),
+				ChangedFiles: github.Int(1),
+			},
+		}
+		gotPRs, err := client.ListPullRequests(ctx, "owner", "repo")
+		if diff := cmp.Diff(wantPRs, gotPRs); diff != "" {
+			t.Errorf("mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("Return status code 500 from GitHub", func(t *testing.T) {
+		t.Parallel()
+
+		token := "test_token"
+		client := NewClient(token)
+		ctx := context.Background()
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			wantURL := "/repos/owner/repo/pulls"
+			if wantURL != req.URL.Path {
+				t.Errorf("mismatch want=%v, got=%s", wantURL, req.URL.Path)
+			}
+
+			wantHTTPMethod := "GET"
+			if wantHTTPMethod != req.Method {
+				t.Errorf("mismatch want=%v, got=%s", wantHTTPMethod, req.Method)
+			}
+
+			rw.WriteHeader(http.StatusInternalServerError)
+			if _, err := rw.Write([]byte("error message")); err != nil {
+				t.Fatal(err)
+			}
+		}))
+		defer testServer.Close()
+
+		testURL, err := url.Parse(testServer.URL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		client.client.BaseURL = testURL
+		if !strings.HasSuffix(client.client.BaseURL.Path, "/") {
+			client.client.BaseURL.Path += "/"
+		}
+
+		// test start
+		_, err = client.ListPullRequests(ctx, "owner", "repo")
+		if err == nil {
+			t.Fatal("expect error occurred, however got nil")
+		}
+
+		var apiError *APIError
+		if !errors.As(err, &apiError) {
+			t.Fatalf("mismatch expect=%T, got=%T", &APIError{}, err)
+		}
+
+		apiErr := err.(*APIError)
+		if apiErr.StatusCode != http.StatusInternalServerError {
+			t.Errorf("mismatch expect=%d, got=%d", apiErr.StatusCode, http.StatusInternalServerError)
+		}
+	})
+
+	t.Run("Return status code 401 from GitHub", func(t *testing.T) {
+		t.Parallel()
+
+		token := "bad_token"
+		client := NewClient(token)
+		ctx := context.Background()
+
+		testServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			wantURL := "/repos/owner/repo/pulls"
+			if wantURL != req.URL.Path {
+				t.Errorf("mismatch want=%v, got=%s", wantURL, req.URL.Path)
+			}
+
+			wantHTTPMethod := "GET"
+			if wantHTTPMethod != req.Method {
+				t.Errorf("mismatch want=%v, got=%s", wantHTTPMethod, req.Method)
+			}
+			respBody, err := json.Marshal([]github.PullRequest{})
+			if err != nil {
+				t.Fatal(err)
+			}
+			rw.WriteHeader(http.StatusUnauthorized)
+			if _, err := rw.Write(respBody); err != nil {
+				t.Fatal(err)
+			}
+		}))
+		defer testServer.Close()
+
+		testURL, err := url.Parse(testServer.URL)
+		if err != nil {
+			t.Fatal(err)
+		}
+		client.client.BaseURL = testURL
+		if !strings.HasSuffix(client.client.BaseURL.Path, "/") {
+			client.client.BaseURL.Path += "/"
+		}
+
+		// test start
+		_, err = client.ListPullRequests(ctx, "owner", "repo")
+		if err == nil {
+			t.Fatal("expect error occurred, however got nil")
+		}
+
+		var apiError *APIError
+		if !errors.As(err, &apiError) {
+			t.Fatalf("mismatch expect=%T, got=%T", &APIError{}, err)
+		}
+
+		apiErr := err.(*APIError)
+		if apiErr.StatusCode != http.StatusUnauthorized {
+			t.Errorf("mismatch expect=%d, got=%d", apiErr.StatusCode, http.StatusUnauthorized)
+		}
+	})
+}
+
+func Test_toDomainModelPR(t *testing.T) {
+	t.Parallel()
+
+	now := time.Date(2023, 2, 24, 12, 34, 56, 0, time.UTC)
+
+	tests := []struct {
+		name     string
+		githubPR *github.PullRequest
+		want     *model.PullRequest
+	}{
+		{
+			name:     "convert empty PR",
+			githubPR: &github.PullRequest{},
+			want:     &model.PullRequest{},
+		},
+		{
+			name: "convert PR without user/time information",
+			githubPR: &github.PullRequest{
+				ID:     github.Int64(1),
+				Number: github.Int(1),
+				State:  github.String("open"),
+				Title:  github.String("test_pr1"),
+				// Without CreatedAt, ClosedAt, MergedAt, User
+				Comments:     github.Int(0),
+				Additions:    github.Int(10),
+				Deletions:    github.Int(5),
+				ChangedFiles: github.Int(2),
+			},
+			want: &model.PullRequest{
+				ID:           github.Int64(1),
+				Number:       github.Int(1),
+				State:        github.String("open"),
+				Title:        github.String("test_pr1"),
+				Comments:     github.Int(0),
+				Additions:    github.Int(10),
+				Deletions:    github.Int(5),
+				ChangedFiles: github.Int(2),
+			},
+		},
+		{
+			name: "convert PR",
+			githubPR: &github.PullRequest{
+				ID:     github.Int64(1),
+				Number: github.Int(1),
+				State:  github.String("open"),
+				Title:  github.String("test_pr1"),
+				CreatedAt: &github.Timestamp{
+					Time: now,
+				},
+				ClosedAt: &github.Timestamp{
+					Time: now,
+				},
+				MergedAt: &github.Timestamp{
+					Time: now,
+				},
+				User: &github.User{
+					Name: github.String("test_user1"),
+				},
+				Comments:     github.Int(0),
+				Additions:    github.Int(10),
+				Deletions:    github.Int(5),
+				ChangedFiles: github.Int(2),
+			},
+			want: &model.PullRequest{
+				ID:           github.Int64(1),
+				Number:       github.Int(1),
+				State:        github.String("open"),
+				Title:        github.String("test_pr1"),
+				CreatedAt:    &model.Timestamp{Time: now},
+				ClosedAt:     &model.Timestamp{Time: now},
+				MergedAt:     &model.Timestamp{Time: now},
+				User:         &model.User{Name: github.String("test_user1")},
+				Comments:     github.Int(0),
+				Additions:    github.Int(10),
+				Deletions:    github.Int(5),
+				ChangedFiles: github.Int(2),
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := toDomainModelPR(tt.githubPR)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
 }
