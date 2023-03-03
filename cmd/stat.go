db3eff67b18bd585c48da0bddde26078ec85f0d4
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-03-03 09:06:24 +0900

    Added line graph output

--- cmd/stat.go
+++ cmd/stat.go
@@ -3,11 +3,15 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"image/color"
 
 	"github.com/nao1215/leadtime/di"
 	"github.com/nao1215/leadtime/domain/usecase"
 	"github.com/shogo82148/pointer"
 	"github.com/spf13/cobra"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func newStatCmd() *cobra.Command {
@@ -64,11 +68,42 @@ func stat(cmd *cobra.Command, args []string) error { //nolint
 	output.LeadTime.RemoveOpenPR()
 
 	if markdown {
+		if err := drawGraph(output.LeadTime); err != nil {
+			return err
+		}
 		outputMarkdown(output.LeadTime)
 		return nil
 	}
 	outputDefault(output.LeadTime)
 
+	return nil
+}
+
+func drawGraph(lt *usecase.LeadTime) error {
+	p := plot.New()
+	p.X.Label.Text = "PR number"
+	p.Y.Label.Text = "Lead Time[min]"
+
+	data := plotter.XYs{}
+	for _, v := range lt.PRs {
+		data = append(data, plotter.XY{
+			X: float64(v.Number),
+			Y: float64(v.MergeTimeMinutes),
+		})
+	}
+
+	line, err := plotter.NewLine(data)
+	if err != nil {
+		return err
+	}
+	p.Add(plotter.NewGrid())
+	line.Color = color.RGBA{R: 226, G: 45, B: 60, A: 255}
+	line.Width = vg.Points(1.5)
+	p.Add(line)
+
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "leadtime.png"); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -83,6 +118,8 @@ func outputMarkdown(lt *usecase.LeadTime) {
 	fmt.Printf("| Lead Time(Sum)|%d[min]|\n", lt.Sum())
 	fmt.Printf("| Lead Time(Ave)|%.2f[min]|\n", lt.Average())
 	fmt.Printf("| Lead Time(MN )|%.2f[min]|\n", lt.Median())
+	fmt.Println()
+	fmt.Println("![PR Lead Time](./leadtime.png)")
 	fmt.Println()
 	fmt.Println("## Pull Request Detail")
 	fmt.Println("| Number | Author | Bot | LeadTime[min] | Title |")
--- go.mod
+++ go.mod
@@ -11,11 +11,18 @@ require (
 	github.com/shogo82148/pointer v1.2.0
 	github.com/spf13/cobra v1.6.1
 	golang.org/x/oauth2 v0.5.0
+	gonum.org/v1/plot v0.12.0
 )
 
 require (
+	git.sr.ht/~sbinet/gg v0.3.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
 	github.com/charmbracelet/lipgloss v0.6.0 // indirect
+	github.com/go-fonts/liberation v0.2.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-pdf/fpdf v0.6.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
@@ -27,8 +34,10 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
 	golang.org/x/net v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
